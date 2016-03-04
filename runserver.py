50444fdbf2f494a3a9d3696ece33d4ddc7ea4ce1
jochemb <j.bijlard@gmail.com>
2016-03-04 17:36:39 +0100

    Brute force port checking is now a reality

--- runserver.py
+++ runserver.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
-from email import message
-
 from arborist import app
-app.run(debug=True)
+import socket
+import argparse
+
+parser = argparse.ArgumentParser(description='Launch the Arborist')
+parser.add_argument('--debug', action="store_true", default=False, help="Run in debug mode")
+parser.add_argument('--port', default=5000, help="Set port to run on.")
+args = parser.parse_args()
+
+
+for port in range(1024, 64000):
+    print('Trying port: ' + str(port))
+    try:
+        app.run(debug=args.debug, port=port)
+        break
+    except socket.error:
+        print('Failed. Trying next:')
