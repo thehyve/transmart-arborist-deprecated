50b099efd5bf490d44c36507af09ea3df77e916a
Jochem Bijlard <j.bijlard@gmail.com>
2016-03-04 03:33:21 -0800

    Added argparse to make debugmode optional

--- runserver.py
+++ runserver.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
-
 from arborist import app
-app.run(debug=True)
+import argparse
+
+parser = argparse.ArgumentParser(description='Launch the Arborist')
+parser.add_argument('--debug', action="store_true", default=False, help="Run in debug mode")
+args = parser.parse_args()
+
+app.run(debug=args.debug)
