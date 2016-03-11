7c8679c09060b7703efea3cbfafa919ae8afc972
jochemb <j.bijlard@gmail.com>
2016-03-11 11:48:33 +0100

    Properly choose port and open in browser.

--- runserver.py
+++ runserver.py
@@ -2,17 +2,28 @@
 from arborist import app
 import socket
 import argparse
+import webbrowser
 
 parser = argparse.ArgumentParser(description='Launch the Arborist')
 parser.add_argument('--debug', action="store_true", default=False, help="Run in debug mode")
-parser.add_argument('--port', default=5000, help="Set port to run on.")
+parser.add_argument('--port', type=int,
+                    help="Set port try to run on. In the case of socket error another is tried.")
 args = parser.parse_args()
 
 
-for port in range(1024, 64000):
-    print('Trying port: ' + str(port))
-    try:
-        app.run(debug=args.debug, port=port)
-        break
-    except socket.error:
-        print('Failed. Trying next:')
+def get_open_port():
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', 0))
+        s.listen(1)
+        port = s.getsockname()[1]
+        s.close()
+        return port
+
+if not args.port:
+    port = get_open_port()
+else:
+    port = args.port
+
+running_on = 'http://localhost:{}'.format(port)
+webbrowser.open(running_on, new=0, autoraise=True)
+app.run(debug=args.debug, port=port)
