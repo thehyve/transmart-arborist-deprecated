ddcce55ee80d7bc85f59f7a37a2c4cf25a8f6f59
jochemb <j.bijlard@gmail.com>
2016-03-11 16:05:20 +0100

    Moved author

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 """
-Author: Jochem Bijlard (j.bijlard@gmail.com)
 Setuptools and py2app/py2exe build script for transmart-arborist.
 
 To install dependencies for your machine:
@@ -130,7 +129,7 @@ setup(
     version='1.3',
     description='Graphical tool to help you prepare your data for loading into the tranSMART data warehouse',
     url='https://github.com/thehyve/transmart-arborist',
-    author='Ward Weistra',
+    author='Ward Weistra and Jochem Bijlard',
     author_email='ward@thehyve.nl',
     license='GNU General Public License V3',
     packages=find_packages(),
